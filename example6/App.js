62995aecefc021ea685645603f8aa7deeb4744ea
dawsonrichey <dawsonrichey89@gmail.com>
2023-10-19 21:25:33 -0400

    added class

--- example6/App.js
+++ example6/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React.createElement => ReactElement-JS Object => HTMLElement (render)
 
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 
-const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
 console.log(heading);
 
+// JSX (transpiled before it reaches the JS) - PARCEL - Babel
+const jsxHeading = <h1 id="heading" className="new">dfgdfgdf</h1>;
 
-// JSX - HTML like or XML like  - is not HTML in JS
-const jsxHeading = <h1 id="heading">dfgdfgdf</h1>;
 console.log(jsxHeading);
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
 root.render(jsxHeading);
