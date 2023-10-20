5189c8047b352678aee43c8c4cf78354063de920
dawsonrichey <dawsonrichey89@gmail.com>
2023-10-20 02:48:15 -0400

    created header and navbar content with logo

--- foodApp/App.js
+++ foodApp/App.js
@@ -1,18 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    Namaste React using JSX
-  </h1>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/burger-logo-design-template-8f3596992e540bb9078f91f59f745ce2_screen.jpg?ts=1653317642"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <h1 className="heaidng">Namaste React Fucntional Component</h1>
-  </div>
-);
+const AppLayout = () => {
+  return <div className="app">
+    <Header />
+  </div>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+
+root.render(<AppLayout />);
