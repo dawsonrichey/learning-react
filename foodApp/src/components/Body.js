9b12926615efa7fce4b8ece3bacf29eda2c52400
dawsonrichey <dawsonrichey89@gmail.com>
2023-10-20 05:16:48 -0400

    extracted components to seperate files

---
+++ foodApp/src/components/Body.js
@@ -0,0 +1,17 @@
+import ResturantCard from "./ResturantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  return (
+    <div className="body" style={{ backgroundColor: "blue" }}>
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resList.map((resturant) => (
+          <ResturantCard key={resturant.info.id} resData={resturant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ foodApp/src/utils/constants.js
@@ -0,0 +1,7 @@
+export const CON_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL = "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/burger-logo-design-template-8f3596992e540bb9078f91f59f745ce2_screen.jpg?ts=1653317642";
+// export default CON_URL;
+// export default LOGO_URL;
+
+// https://d1csarkz8obe9u.cloudfront.net/posterpreviews/burger-logo-design-template-8f3596992e540bb9078f91f59f745ce2_screen.jpg?ts=1653317642
