5737a9183f89900980b48e6f3ab0ad1c4d13e5ce
dawsonrichey <dawsonrichey89@gmail.com>
2023-10-19 20:47:10 -0400

    no use of cdn using import modules

--- example5/App.js
+++ example5/App.js
@@ -1,7 +1,23 @@
-const heading = React.createElement("h1", {id:"heading", special:"mine"}, "hello world, from dawson");
+import React from "react";
+import ReactDOM from "react-dom";
 
-console.log(heading); //object
+const parent = React.createElement ("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+]),
+    React.createElement("div", { id: "child?" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+]),
+]);
+
+React.createElement("h1", {id:"heading", special:"mine"}, "hello world, from dawson");
+
+console.log(parent); //object
+// console.log(heading); //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
+// root.render(heading);
--- example5/dist/index.6bd02f5a.js
+++ example5/dist/index.6bd02f5a.js
@@ -1,9 +1,26 @@
-const heading = React.createElement("h1", {
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", fh, "I'm an h1 tag"),
+        React.createElement("h2", fh, "I'm an h2 tag")
+    ]),
+    React.createElement("div", {
+        id: "child?"
+    }, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", f, "I'm an h2 tag")
+    ])
+]);
+React.createElement("h1", {
     id: "heading",
     special: "mine"
 }, "hello world, from dawson");
-console.log(heading); //object
+console.log(parent); //object
+// console.log(heading); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent); // root.render(heading);
 
 //# sourceMappingURL=index.6bd02f5a.js.map
