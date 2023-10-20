266f0656c241c355eed00fcb79ef5ed41fed5437
dawsonrichey <dawsonrichey89@gmail.com>
2023-10-20 03:16:01 -0400

    added multiple cards

--- foodApp/App.js
+++ foodApp/App.js
@@ -22,23 +22,43 @@ const Header = () => {
   );
 };
 
-const styleCard ={
-    backgroundColor: "yellow",
+const styleCard = {
+  backgroundColor: "yellow",
 };
 
 const ResturantCard = () => {
   return (
     <div className="res-card" style={styleCard}>
       <h3>Meghana Foods</h3>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/Biryani_2.png"
+      ></img>
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4 stars</h4>
+      <h4>38min</h4>
     </div>
   );
 };
 
 const Body = () => {
   return (
-    <div className="body" style={{backgroundColor: "blue"}}>
+    <div className="body" style={{ backgroundColor: "blue" }}>
       <div className="search">Search</div>
-      <div className="res-conatainer">
+      <div className="res-container">
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
         <ResturantCard />
       </div>
     </div>
