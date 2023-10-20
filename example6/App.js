5e46a1f740521d95649c3e1eda26e2cb0f9fc48a
dawsonrichey <dawsonrichey89@gmail.com>
2023-10-19 21:21:24 -0400

    started using JSX

--- example6/App.js
+++ example6/App.js
@@ -3,3 +3,16 @@ import ReactDOM from "react-dom/client";
 
 
 
+const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+console.log(heading);
+
+
+// JSX - HTML like or XML like  - is not HTML in JS
+const jsxHeading = <h1 id="heading">dfgdfgdf</h1>;
+console.log(jsxHeading);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+root.render(jsxHeading);
