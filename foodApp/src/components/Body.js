3dbdefab0efc5bc4ea8df7fecbda16ae7c171db5
dawsonrichey <dawsonrichey89@gmail.com>
2023-10-23 02:42:10 -0400

    created filter button

--- foodApp/src/components/Body.js
+++ foodApp/src/components/Body.js
@@ -2,11 +2,82 @@ import ResturantCard from "./ResturantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
+  let listOfRestaurants = [
+    {
+    info: {
+      id: "334475",
+      name: "KFC",
+      cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      costForTwo: 40000,
+      deliveryTime: 36,
+      avgRating: "3.8",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 0.1,
+        serviceability: "SERVICEABLE",
+        slaString: "19 mins",
+        lastMileTravelString: "0.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      }
+      },
+      
+  },
+  {
+    info: {
+      id: "334275",
+      name: "Domino",
+      cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      costForTwo: 40000,
+      deliveryTime: 36,
+      avgRating: "8",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 0.1,
+        serviceability: "SERVICEABLE",
+        slaString: "19 mins",
+        lastMileTravelString: "0.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      }
+      },
+      
+  },
+  {
+    info: {
+      id: "3342775",
+      name: "Dom",
+      cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      costForTwo: 40000,
+      deliveryTime: 36,
+      avgRating: "8",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 0.1,
+        serviceability: "SERVICEABLE",
+        slaString: "19 mins",
+        lastMileTravelString: "0.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      }
+      },
+      
+  },
+
+];
   return (
     <div className="body" style={{ backgroundColor: "blue" }}>
-      <div className="search">Search</div>
+      <div className="filter">
+      <button className="filter-btn" 
+      onClick={() => {
+        // Filter Logic here
+        listOfRestaurants = listOfRestaurants.filter(
+        (res) => res.info.avgRating > 4);
+        console.log(listOfRestaurants);
+      }}>Top Rated Restaurants button</button>
+        </div>
       <div className="res-container">
-        {resList.map((resturant) => (
+        {listOfRestaurants.map((resturant) => (
           <ResturantCard key={resturant.info.id} resData={resturant} />
         ))}
       </div>
