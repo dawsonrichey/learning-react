9b96f103f720a4b8840b713320941eeb001b1461
dawsonrichey <dawsonrichey89@gmail.com>
2023-10-27 08:28:15 -0400

    multiple cards

--- foodApp/src/components/Body.js
+++ foodApp/src/components/Body.js
@@ -6,94 +6,7 @@ import { useState } from "react";
 
 const Body = () => {
 // Local State Variable * Super powerful variable
-const [listOfRestaurants] = useState([   
-   {
-  info: {
-    id: "334475",
-    name: "KFC",
-    cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-    cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-    costForTwo: 40000,
-    deliveryTime: 36,
-    avgRating: "3.8",
-    sla: {
-      deliveryTime: 19,
-      lastMileTravel: 0.1,
-      serviceability: "SERVICEABLE",
-      slaString: "19 mins",
-      lastMileTravelString: "0.1 km",
-      iconType: "ICON_TYPE_EMPTY",
-    },
-    },
-  }
-    
-]);
-// let listOfRestaurants = null;
-// Normal JS Variable
-//   let listOfRestaurants = [
-//     {
-//     info: {
-//       id: "334475",
-//       name: "KFC",
-//       cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-//       cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-//       costForTwo: 40000,
-//       deliveryTime: 36,
-//       avgRating: "3.8",
-//       sla: {
-//         deliveryTime: 19,
-//         lastMileTravel: 0.1,
-//         serviceability: "SERVICEABLE",
-//         slaString: "19 mins",
-//         lastMileTravelString: "0.1 km",
-//         iconType: "ICON_TYPE_EMPTY",
-//       }
-//       },
-      
-//   },
-//   {
-//     info: {
-//       id: "334275",
-//       name: "Domino",
-//       cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-//       cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-//       costForTwo: 40000,
-//       deliveryTime: 36,
-//       avgRating: "8",
-//       sla: {
-//         deliveryTime: 19,
-//         lastMileTravel: 0.1,
-//         serviceability: "SERVICEABLE",
-//         slaString: "19 mins",
-//         lastMileTravelString: "0.1 km",
-//         iconType: "ICON_TYPE_EMPTY",
-//       }
-//       },
-      
-//   },
-//   {
-//     info: {
-//       id: "3342775",
-//       name: "Dom",
-//       cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-//       cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-//       costForTwo: 40000,
-//       deliveryTime: 36,
-//       avgRating: "8",
-//       sla: {
-//         deliveryTime: 19,
-//         lastMileTravel: 0.1,
-//         serviceability: "SERVICEABLE",
-//         slaString: "19 mins",
-//         lastMileTravelString: "0.1 km",
-//         iconType: "ICON_TYPE_EMPTY",
-//       }
-//       },
-      
-//   },
-
-// ];
-  let listOfRestaurantsJS = [
+let [listOfRestaurants] = useState([
     {
     info: {
       id: "334475",
@@ -155,9 +68,11 @@ const [listOfRestaurants] = useState([
       
   },
 
-];
+]);
+
+
   return (
-    <div className="body" style={{ backgroundColor: "blue" }}>
+    <div className="body">
       <div className="filter">
       <button className="filter-btn" 
       onClick={() => {
